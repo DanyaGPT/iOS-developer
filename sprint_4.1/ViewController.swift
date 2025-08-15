9babed5fef99ec32b948948c4a7e4d84a15963dc
DanyaGPT <zorind123@gmail.com>
2025-08-15 23:52:36 +0900

    Initial Commit

---
+++ sprint_4.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprint_4.1
+//
+//  Created by Зорин Даниил on 15.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sprint_4.1Tests/sprint_4_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  sprint_4_1Tests.swift
+//  sprint_4.1Tests
+//
+//  Created by Зорин Даниил on 15.08.2025.
+//
+
+import Testing
+@testable import sprint_4_1
+
+struct sprint_4_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
